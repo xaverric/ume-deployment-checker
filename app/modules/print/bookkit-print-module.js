d461c9daa82540f5b179009db61dc926080bb6ce
Daniel Jílek <jilek.daniel@gmail.com>
2022-09-06 10:33:40 +0200

    fix header formatting

--- app/modules/print/bookkit-print-module.js
+++ app/modules/print/bookkit-print-module.js
@@ -12,7 +12,7 @@ const uu5StringTemplate = (rows, columns, header) => {
     return `<uu5string/>
         <UU5.Bricks.Lsi>
             <UU5.Bricks.Lsi.Item language="en">
-                <UU5.Bricks.Section contentEditable level="3" header="${header}" colorSchema=null>
+                <UU5.Bricks.Section contentEditable level="3" header="${header.toUpperCase()}" colorSchema=null>
                     <UuContentKit.Bricks.BlockDefault>
                         <UU5.RichText.Block uu5string="Last update on ${new Date()}"/>
                     </UuContentKit.Bricks.BlockDefault>
